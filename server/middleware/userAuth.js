61373818f638490711c9ff7695945488429686d8
Anugrah <your-email@example.com>
2025-12-15 11:32:51 +0530

    fix(auth): correct user role validation in userAuth middleware

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
-const jwtSecret = process.env.JWT_SECRET;
+const accessTokenSecret = process.env.ACCESS_TOKEN_PRIVATE_KEY;
 
 const userAuth = async (req, res, next) => {
   const authHeader = req.header("Authorization");
@@ -15,11 +15,14 @@ const userAuth = async (req, res, next) => {
   const token = authHeader.replace("Bearer ", "");
 
   try {
-    const decode = jwt.verify(token, jwtSecret);
-    const userId = decode.user.id;
+    const decode = jwt.verify(token, accessTokenSecret);
+    const userId = decode._id;
     const user = await User.findById(userId).select("role");
+    if (!user) {
+        return res.status(401).send({ error: "User not found." });
+    }
     console.log("role", user);
-    if (user.role !== "user") {
+    if (!user.role.includes("user")) {
       return res
         .status(403)
         .send({ error: "Forbidden. User privileges required." });
