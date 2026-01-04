8ce7b13ed8e13f7e824c6d5980f88ac6ebbbb361
ANUGRAH KARIYATT <162654910+Anugrah71@users.noreply.github.com>
2026-01-04 18:16:38 +0530

    Import useAuth from AuthContext in Cart component

--- client/src/Screens/Cart.jsx
+++ client/src/Screens/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Delete from "@mui/icons-material/Delete";
 import { useCart, useDispatchCart } from "../context/ContextReducer";
+import { useAuth } from "../context/AuthContext";
+
 import api from "../api/axios";
 
 export default function Cart() {
